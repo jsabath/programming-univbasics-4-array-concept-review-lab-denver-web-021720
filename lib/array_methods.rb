2e62b6180fd910bbae5670d864d8d8cbb611e2a5
Justin Sabath <justin.sabath@aol.com>
2020-01-14 22:53:35 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   array = []
   length = array.length
   counter = 0
-  while counter < array.length do |index|
+  length.times < array.length do |index|
     puts array[index]
     counter += 1
   end
